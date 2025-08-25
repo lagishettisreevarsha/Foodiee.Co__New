5fe909cd533f2a9186967a94aa73aa117b6c9f43
SREE VARSHA LAGISHETTI <sreevarsha.lagishetti11@gmail.com>
2025-08-25 12:20:37 +0530

    Updated home component and other files

--- src/app/food-detail/food-detail.component.ts
+++ src/app/food-detail/food-detail.component.ts
@@ -13,12 +13,14 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class FoodDetailComponent implements OnInit{
   foodItem:any;
+  safeYoutubeUrl?: SafeResourceUrl;
   constructor(private router: Router, private route: ActivatedRoute, private foodService: FoodService, private sanitizer: DomSanitizer) {}
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.foodService.getFoods().subscribe(data => {
         this.foodItem = data.find(item => item.id == +id);
+        this.prepareVideo();
          });
     }
   }
@@ -28,7 +30,27 @@ export class FoodDetailComponent implements OnInit{
 }
   
 getSafeYoutubeUrl(url: string): SafeResourceUrl {
-  const videoId = url.split('v=')[1] || url.split('youtu.be/')[1];
-  return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`);
+  // Handle common YouTube URL formats
+  try {
+    let videoId = '';
+    if (url.includes('youtube.com/watch')) {
+      const u = new URL(url);
+      videoId = u.searchParams.get('v') || '';
+    } else if (url.includes('youtu.be/')) {
+      videoId = url.split('youtu.be/')[1].split(/[?&#]/)[0];
+    } else if (url.includes('youtube.com/embed/')) {
+      videoId = url.split('embed/')[1].split(/[?&#]/)[0];
+    }
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`);
+  } catch {
+    return this.sanitizer.bypassSecurityTrustResourceUrl('about:blank');
+  }
+}
+
+private prepareVideo() {
+  const yt = this.foodItem?.youtubeUrl || this.foodItem?.youtube || this.foodItem?.videoUrl;
+  if (yt) {
+    this.safeYoutubeUrl = this.getSafeYoutubeUrl(yt);
+  }
 }
 }
